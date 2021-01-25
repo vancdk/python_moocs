fea2c43dcaed7c95931e7bbb59a7729b7d48b715
vancdk <74259267+vancdk@users.noreply.github.com>
2021-01-25 14:59:20 +0100

    Update 04_tuples.py
    update tuple definition

--- coursera/crash_course_on_python_by_google/04_tuples.py
+++ coursera/crash_course_on_python_by_google/04_tuples.py
@@ -1,5 +1,7 @@
 # Tuples can contain elements of any data type, but unlike lists, they are immutable.
 # They’re specified using parentheses instead of square brackets.
+# Storing the elements of a tuple in separate variables is called unpacking. 
+# This allows us to take multiple returned values from a function and store each value in its own variable.
 
 # We can use tuples to store information about a file: its name, its type and its size in bytes. 
 # file_size() returns the size in kilobytes (a kilobyte is 1024 bytes) up to 2 decimal places. 
