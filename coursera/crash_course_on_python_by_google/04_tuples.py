3915c669bb4779780ec58088f69523c9d9b5f8f1
vancdk <74259267+vancdk@users.noreply.github.com>
2021-01-25 14:57:38 +0100

    Update 04_tuples.py
    add tuple definition

--- coursera/crash_course_on_python_by_google/04_tuples.py
+++ coursera/crash_course_on_python_by_google/04_tuples.py
@@ -1,3 +1,6 @@
+# Tuples can contain elements of any data type, but unlike lists, they are immutable.
+# They’re specified using parentheses instead of square brackets.
+
 # We can use tuples to store information about a file: its name, its type and its size in bytes. 
 # file_size() returns the size in kilobytes (a kilobyte is 1024 bytes) up to 2 decimal places. 
 
