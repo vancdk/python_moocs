225d3c31f819496bcc970b9b59520a34bb54e3e1
vancdk <74259267+vancdk@users.noreply.github.com>
2021-01-25 14:55:50 +0100

    Create 04_tuples.py

---
+++ coursera/crash_course_on_python_by_google/04_tuples.py
@@ -0,0 +1,13 @@
+# We can use tuples to store information about a file: its name, its type and its size in bytes. 
+# file_size() returns the size in kilobytes (a kilobyte is 1024 bytes) up to 2 decimal places. 
+
+def file_size(file_info):
+	file_name, file_type, file_size = file_info
+	return("{:.2f}".format(file_size / 1024))
+
+print(file_size(('Class Assignment', 'docx', 17875))) # Should print 17.46
+print(file_size(('Notes', 'txt', 496))) # Should print 0.48
+print(file_size(('Program', 'py', 1239))) # Should print 1.21
+
+
+
