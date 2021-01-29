50c05a80aa5443cfbe93eb4cdacf2f9708e6433e
vancdk <74259267+vancdk@users.noreply.github.com>
2021-01-29 12:42:26 +0100

    Create 05_inheritence.py

---
+++ coursera/crash_course_on_python_by_google/05_inheritence.py
@@ -0,0 +1,14 @@
+# The class Shirt inherits methods from the Clothing class
+
+class Clothing:
+  material = ""
+  def __init__(self,name):
+    self.name = name
+  def checkmaterial(self):
+	  print("This {} is made of {}".format(self.name,self.material))
+			
+class Shirt(Clothing):
+  material="Cotton"
+
+polo = Shirt("Polo")
+polo.checkmaterial()
