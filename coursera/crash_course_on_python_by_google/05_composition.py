4badb61336edde8f7fe7a99ace602efd225c79ee
vancdk <74259267+vancdk@users.noreply.github.com>
2021-01-29 19:02:53 +0100

    Create 05_composition.py

---
+++ coursera/crash_course_on_python_by_google/05_composition.py
@@ -0,0 +1,34 @@
+# Composition is a situation where two different classes are related, but there is no inheritance going on.
+# Composition allows us to use objects as attributes, as well as access all their attributes and methods.
+
+# The "Stock_by_Material" method iterates over the amount of each item of a given material that is in stock. 
+
+class Clothing:
+  stock={ 'name': [],'material' :[], 'amount':[]}
+  def __init__(self,name):
+    material = ""
+    self.name = name
+  def add_item(self, name, material, amount):
+    Clothing.stock['name'].append(self.name)
+    Clothing.stock['material'].append(self.material)
+    Clothing.stock['amount'].append(amount)
+  def Stock_by_Material(self, material):
+    count=0
+    n=0
+    for item in Clothing.stock['material']:
+      if item == material:
+        count += Clothing.stock['amount'][n]
+        n+=1
+    return count
+
+class shirt(Clothing):
+  material="Cotton"
+class pants(Clothing):
+  material="Cotton"
+  
+polo = shirt("Polo")
+sweatpants = pants("Sweatpants")
+polo.add_item(polo.name, polo.material, 4)
+sweatpants.add_item(sweatpants.name, sweatpants.material, 6)
+current_stock = polo.Stock_by_Material("Cotton")
+print(current_stock)
