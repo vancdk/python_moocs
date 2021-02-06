8f00640b74dd9941a725f3abbdfb18bd5012bbb0
vancdk <74259267+vancdk@users.noreply.github.com>
2021-02-06 14:53:10 +0100

    Review 03_for_loops.py

--- coursera/crash_course_on_python_by_google/03_for_loops.py
+++ coursera/crash_course_on_python_by_google/03_for_loops.py
@@ -1,15 +1,15 @@
-# Makes the factorial function return the factorial of n. 
-# Then, prints the first 10 factorials (from 0 to 9) with the corresponding number. 
+# Make the factorial function return the factorial of n. 
+# Then, print the first 10 factorials (from 0 to 9) with the corresponding number. 
 # Remember: the factorial of a number is defined as the product of an integer and all integers before it.
 
 def factorial(n):
   result = 1
-  for x in range(1,n):
+  for x in range(1,n+1):
     result = result * x
   return result
 
 for n in range(0,10):
-  print(n, factorial(n+1))
+  print(n, factorial(n))
   
 # Result: 
 # 0 1
