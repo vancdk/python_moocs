8fa7cf5cab2bce83d54f0d793b3369ba3389b677
vancdk <74259267+vancdk@users.noreply.github.com>
2021-02-11 15:04:19 +0100

    Create run_a_python_script.sh

---
+++ coursera/interact_with_the_operating_system_by_google/run_a_python_script.sh
@@ -0,0 +1,14 @@
+# Display content of a file
+cat my_file.py
+
+# Execute a script
+python3 my_file.py
+
+# Add a shebang in a script file
+nano my_file.py # Add this at the top of the script before saving the file: #!usr/bin/env python3
+
+# Change the files permissions to make it executable
+chmod +x my_file.py
+
+# We can now run the file by prefixing it with a ./
+./my_file.py
