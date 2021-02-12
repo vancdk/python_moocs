30ec89af95fd301d55502f0331286520ddd18f07
vancdk <74259267+vancdk@users.noreply.github.com>
2021-02-12 11:53:31 +0100

    Update and rename read_files.py to read_a_file.py

--- coursera/interact_with_the_operating_system_by_google/read_files.py
+++
@@ -1,9 +0,0 @@
-
-# Python will automatically close the file at the end of a with block
-with open("my_file.txt") as file:
-  # The readline() method reads a single line from the current position
-  print(file.readline())
-  # The read() method reads until the end of the file, also from the current position
-  print(file.read())
-  
- 
