53b18b472dd166ac258163f28b3790f987b09380
vancdk <74259267+vancdk@users.noreply.github.com>
2021-02-12 11:56:32 +0100

    Update read_a_file.py
    reorder blocks of code

--- coursera/interact_with_the_operating_system_by_google/read_a_file.py
+++ coursera/interact_with_the_operating_system_by_google/read_a_file.py
@@ -1,24 +1,11 @@
-# Python will automatically close the file at the end of a with block
-with open("my_file.txt") as file:
-  # The readline() method reads a single line from the current position
-  print(file.readline())
-  # The read() method reads until the end of the file, also from the current position
-  print(file.read())
-  
-  
 """
 Ways of iterating through the contents of a file.
 
 If a file is very large, it's more memory efficient to process it line by line.
 """
-# 1 
-with open("my_file.txt") as file:
-  for line in file:
-    # Use strip() to avoid printing empty lines by eliminating the new line characters
-    print(line.strip().upper())
-      
-# 2
+# 1
 file = open("my_file.txt")
+# The readline() method reads a single line from the current position
 lines = file.readlines()
 file.close()
 lines.sort()
@@ -28,3 +15,15 @@ This will print:
  the lines sorted alphabetically
  "\n" for new line characters and "\t" for tabs
 """
+
+# 2
+# Python will automatically close the file at the end of a with block
+with open("my_file.txt") as file:
+  # The read() method reads until the end of the file, also from the current position
+  print(file.read())
+  
+# 3
+with open("my_file.txt") as file:
+  for line in file:
+    # Use strip() to avoid printing empty lines by eliminating the new line characters
+    print(line.strip().upper())
