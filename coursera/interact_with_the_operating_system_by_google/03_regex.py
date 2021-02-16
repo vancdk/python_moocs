513f79ded5ede6c022d3bf3684c3e81e35161678
vancdk <74259267+vancdk@users.noreply.github.com>
2021-02-16 20:41:47 +0100

    Update 03_regex.py

--- coursera/interact_with_the_operating_system_by_google/03_regex.py
+++ coursera/interact_with_the_operating_system_by_google/03_regex.py
@@ -10,3 +10,30 @@ regex = r"\[(\d+)\]"
 result = re.search(regex, log)
 print(result[1])
 
+
+# The r at the beginning indicates that this is a rawstring
+result = re.search(r"aza", "plaza")
+print(result)
+# Prints: <re.Match object; span=(2, 5), match='aza'>
+
+result = re.search(r"aza", "bazaar")
+print(result)
+# Prints: <re.Match object; span=(1, 4), match='aza'>
+
+result = re.search(r"aza", "maze")
+print(result)
+# Prints: None
+
+print(re.search(r"^x","xenon"))
+# Prints: <re.Match object; span=(0, 1), match='x'>
+
+print(re.search(r"p.ng","penguin"))
+# Prints: <re.Match object; span=(0, 4), match='peng'>
+
+print(re.search(r"p.ng","sponge"))
+# Prints: <re.Match object; span=(1, 5), match='pong'>
+
+
+# Additional options
+print(re.search(r"p.ng","Pangaea", re.IGNORECASE))
+# Prints: <re.Match object; span=(0, 4), match='Pang'>
