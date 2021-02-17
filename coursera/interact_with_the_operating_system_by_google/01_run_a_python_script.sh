324d917b5aae72cf86bf10625973288dfd636683
vancdk <74259267+vancdk@users.noreply.github.com>
2021-02-17 12:05:53 +0100

    Update 01_run_a_python_script.sh

--- coursera/interact_with_the_operating_system_by_google/01_run_a_python_script.sh
+++ coursera/interact_with_the_operating_system_by_google/01_run_a_python_script.sh
@@ -5,10 +5,10 @@ cat my_file.py
 python3 my_file.py
 
 # Add a shebang in a script file
-nano my_file.py # Add this at the top of the script before saving the file: #!usr/bin/env python3
+nano my_file.py # Add at the top before saving the file: #!usr/bin/env python3
 
 # Change the files permissions to make it executable
 chmod +x my_file.py
 
-# We can now run the file by prefixing it with a ./
+# You can now run the file by prefixing it with a ./
 ./my_file.py
