e3d2423688ffc01c401ed9a32e84d7dbcaaf76af
vancdk <74259267+vancdk@users.noreply.github.com>
2021-03-23 16:32:49 +0100

    Update 04_filtering_log_files.py

--- coursera/interact_with_the_operating_system_by_google/04_filtering_log_files.py
+++ coursera/interact_with_the_operating_system_by_google/04_filtering_log_files.py
@@ -3,11 +3,19 @@ import re
 import sys
 
 logfile = sys.argv[1]
+usernames = {}
+
 with open(logfile) as f:
   for line in f:
     if "CRON" not in line:
       continue
     pattern = r"USER \((\w+)\)$"
     result = re.search(pattern, line)
-    print(line.strip())
+    if result is None:
+      continue
+    name = result[1]
+    usernames[name] = usernames.get(name, 0) + 1
+
+print(usernames)
     
+
