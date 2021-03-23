549cb87dd1024d35169d4543a5a162ff858b0d5e
vancdk <74259267+vancdk@users.noreply.github.com>
2021-03-23 16:29:00 +0100

    Create 04_filtering_log_files.py

---
+++ coursera/interact_with_the_operating_system_by_google/04_filtering_log_files.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import re
+import sys
+
+logfile = sys.argv[1]
+with open(logfile) as f:
+  for line in f:
+    if "CRON" not in line:
+      continue
+    pattern = r"USER \((\w+)\)$"
+    result = re.search(pattern, line)
+    print(line.strip())
+    
