5a3523d6d4d20932803f7c250e7f883759a9f660
vancdk <74259267+vancdk@users.noreply.github.com>
2021-03-23 14:57:39 +0100

    Update 04_variables.sh

--- coursera/interact_with_the_operating_system_by_google/04_variables.sh
+++ coursera/interact_with_the_operating_system_by_google/04_variables.sh
@@ -40,3 +40,30 @@ $ wc variables.py
 
 $ echo $?
 0 # the exit value is == 0 because the wc command ran successfully
+
+$ cat create_file.py 
+#!/usr/bin/env python3
+
+import os
+import sys
+
+filename=sys.argv[1]
+
+if not os.path.exists(filename):
+  with open(filename, "w") as f:
+    f.write("New file created\n")
+else:
+  print("Error, the file {} already exists!".format(filename))
+  sys.exit(1)
+
+$ ./create_file.py example
+$ echo $?
+0
+
+$ cat example
+New file created
+
+$ ./create_file.py example
+Error, the file example already exists!
+
+
